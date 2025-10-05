754faa8f8082bc7d1499db2f44c144ba282d96b9
Mathias Markl <mathias.markl@mukit.at>
2025-10-05 19:28:08 +0200

    update

--- muk_contacts_vcard/__manifest__.py
+++ muk_contacts_vcard/__manifest__.py
@@ -10,7 +10,7 @@
     'license': 'LGPL-3', 
     'author': 'MuK IT',
     'website': 'http://www.mukit.at',
-    'live_test_url': 'https://my.mukit.at/r/f6m',
+    'live_test_url': 'https://youtu.be/j_iZRgJnOGk',
     'contributors': [
         'Mathias Markl <mathias.markl@mukit.at>',
     ],
